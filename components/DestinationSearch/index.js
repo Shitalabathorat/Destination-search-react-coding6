c335ba41004a2d9d77d6b72a92dda25d93642422
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-03-27 15:19:10 +0530

    Update index.js

--- components/DestinationSearch/index.js
+++ components/DestinationSearch/index.js
@@ -1,18 +1,29 @@
-// Write your code here
+
 import {Component} from 'react'
 import './index.css'
 import Destination from './components/DestinationItem'
-import destinationsList from './App.js'
+import initialDestinationsList from './App.js'
 
 class DestinationSearch extends Component {
+    state={
+        searchInput: ''
+        destinationsList: initialDestinationsList 
+    }
+    onChangeSearchInput= event=>{
+        this.setState({searchInput: event.target.value})
+    }
+   
   render() {
+      const {searchInput,destinationsList}=this.state
+
+      const searchResults=destinationsList.filter(eachUser=>eachUser.name.includes(searchInput),)
     return (
       <div class="app-container">
         <h1 className="head">Destination Search</h1>
-        <input type="search" />
+        <input type="search" value={searchInput} onChange={this.onChangeSearchInput}/>
         <ul className="list-container">
-          {destinationsList.map(eachItem => (
-            <Destination destinationDetails={eachItem} key={eachItem.id} />
+          {searchResults.map(eachItem => (
+            <Destination destinationDetails={eachUser} deleteUser={eachUser} key={eachItem.id} />
           ))}
         </ul>
       </div>
